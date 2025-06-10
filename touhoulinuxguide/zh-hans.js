107beab14b8bbd7ee961bbe3e6e18b8e2d05487f
David Liu <77701856+daichungus@users.noreply.github.com>
2025-06-09 20:23:38 -0700

    Added zh-hans.js

---
+++ touhoulinuxguide/zh-hans.js
@@ -0,0 +1,21 @@
+{
+  "dat_dump": false,
+  "patched_files_dump": false,
+  "patches": [
+    {
+      "archive": "repos/nmlgc/base_tsa/"
+    },
+    {
+      "archive": "repos/nmlgc/base_tasofro/"
+    },
+    {
+      "archive": "repos/tpZHCNex/taso/"
+    },
+    {
+      "archive": "repos/tpZHCNex/tsa/"
+    },
+    {
+      "archive": "repos/thpatch/lang_zh-hans/"
+    }
+  ]
+}
