781575b6860f5276932859f80bab9c17037307b1
David Liu <77701856+daichungus@users.noreply.github.com>
2025-06-09 20:34:51 -0700

    added 2 more examples

---
+++ touhoulinuxguide/stack_example.js
@@ -0,0 +1,17 @@
+{
+  "dat_dump": false,
+  "patched_files_dump": false,
+  "patches": [
+    {
+      "archive": "repos/nmlgc/base_tsa/" // This is loaded first, all patches require it.
+    },
+    {
+      "archive": "repos/daichungus/never_starts/" // The OST but the tracks will get stuck in a limbo
+    },
+    {
+      "archive": "repos/daichungus/patchy_chungus/" // Replaces Patchouli with Big Chungus in EoSD
+    }
+  ]
+}
+
+// Now I can have both a cursed soundtrack and Big Chungus!
